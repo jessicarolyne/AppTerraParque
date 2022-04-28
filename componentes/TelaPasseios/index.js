85f7ff05c7cd6644a9f323f4ad961cebe1d1dd42
jessicarolyne <jessicarolyne@hotmail.com>
2022-04-27 21:28:34 -0300

    Desenvolvimento da tela Passeios

--- componentes/TelaPasseios/index.js
+++ componentes/TelaPasseios/index.js
@@ -1,8 +1,30 @@
 import React from 'react';
-import {Text} from 'react-native';
+import {View, Text, Image} from 'react-native';
+import estilos from '../TelaPasseios/estilos';
 
-export default function TelaPasseios(){
+import arena from '../../assets/images/arena-paintball.jpg';
+import arborismo from '../../assets/images/arborismo-com-tirolesa.jpg';
+import passeio from '../../assets/images/passeio-a-cavalo.jpg';
+
+export default function TelaRestaurantes(){
     return (
-        <Text>Tela Passeios</Text>
+        <View style={estilos.container}>
+            <View style={estilos.conteudo}>
+                <Text style={estilos.titulo}>Passeios</Text>
+                <Text style={estilos.texto}>No Terra Parque, o combate é realizado entre equipes de, no mínimo, dois integrantes cada...</Text>
+            </View>
+            <View style={estilos.card}>
+                <Text style={estilos.titulocard}>Arena paintball</Text>
+                <Image source={arena} style={estilos.imagem}/>
+            </View>
+            <View style={estilos.card}>
+                <Text style={estilos.titulocard}>arborismo com tirolesa</Text>
+                <Image source={arborismo} style={estilos.imagem}/>
+            </View>
+            <View style={estilos.card}>
+                <Text style={estilos.titulocard}>Passeio a cavalo</Text>
+                <Image source={passeio} style={estilos.imagem}/>
+            </View>
+        </View>
     )
 }
